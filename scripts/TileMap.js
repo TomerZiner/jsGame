2ff612667b270e27df188a6091cf8ceb4d2ca774
Your Name <you@example.com>
2025-03-19 09:38:59 +0200

    pacman_added

--- scripts/TileMap.js
+++ scripts/TileMap.js
@@ -1,52 +1,83 @@
+import pacman from "./pacman.js";
+
 export default class TileMap {
-    constructor(tileSize) {
-        this.tileSize = tileSize;
+  constructor(tileSize) {
+    this.tileSize = tileSize;
 
-        this.yellowDot = new Image();
-        this.yellowDot.src = "./images/yellowdot.png";
+    this.yellowDot = new Image();
+    this.yellowDot.src = "./images/yellowdot.png";
 
-        this.wall = new Image();
-        this.wall.src = "./images/wall.png";
-    }
+    this.wall = new Image();
+    this.wall.src = "./images/wall.png";
+  }
 
-    map = [
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1],
-    ];
+  map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
 
-    draw(ctx) {
-        for (let row = 0; row < this.map.length; row++) {
-            for (let column = 0; column < this.map[row].length; column++) {
-                let tile = this.map[row][column];
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
 
-                if (tile === 1) {
-                    this.#drawWall(ctx, column, row, this.tileSize);
-                }
-                else if (tile === 0) {
-                    this.#drawWall(ctx, column, row, this.tileSize);
-                }
-            }
+        if (tile === 1) {
+          this.#drawWall(ctx, column, row, this.tileSize);
+        } else if (tile === 0) {
+          this.#drawDot(ctx, column, row, this.tileSize);
         }
+        ctx.strokeStyle = "yellow";
+        ctx.strokeRect(
+          column * this.tileSize,
+          row * this.tileSize,
+          this.tileSize,
+          this.tileSize
+        );
+      }
     }
+  }
 
+  #drawDot(ctx, column, row, size) {
+    ctx.drawImage(
+      this.yellowDot,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
+  #drawWall(ctx, column, row, size) {
+    ctx.drawImage(this.wall, column * size, row * size, size, size);
+  }
 
-    #drawWall(ctx, column, row, size) {
-        ctx.drawImage(this.wall, column * size, row * size, size, size);
-    }
+  getPacman(velocity) {
+    for (let row = 0; row < this.map.length; row++)
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 4) {
+          this.map[row][column] = 0;
+          return new pacman(
+            column * this.tileSize,
+            row * this.tileSize,
+            this.tileSize,
+            velocity,
+            this
+          );
+        }
+      }
+  }
 
-    setCanvasSize(canvas) {
-        canvas.width = this.map[0].length * this.tileSize;
-        canvas.height = this.map.length * this.tileSize;
-    }
+  setCanvasSize(canvas) {
+    canvas.width = this.map[0].length * this.tileSize;
+    canvas.height = this.map.length * this.tileSize;
+  }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,11 @@
 import TileMap from "./TileMap.js";
 const tileSize = 32;
+const  velocity = 1;
+
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext('2d');
 const tileMap = new TileMap(tileSize);
+const pacman = tileMap.getPacman(velocity);
 
 
 function gameLoop() {
